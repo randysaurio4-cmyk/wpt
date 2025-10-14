c81640738bdea096fa4a2dc9af4dcaf1c8958259
James Graham <james@hoppipolla.co.uk>
2025-10-14 11:14:30 +0100

    Set logcat dir

--- tools/ci/taskcluster-run.py
+++ tools/ci/taskcluster-run.py
@@ -80,7 +80,6 @@ def main(product, channel, commit_range, artifact_path, wpt_args):
         "--no-pause",
         "--no-restart-on-unexpected",
         "--verify-log-full",
-        "--setpref='remote.log.level=Trace'"
     ]
     # Enable headless mode for WPE MiniBrowser because it can't work under Xvfb/X11 (needs Wayland)
     wpt_args.append("--headless" if product == "wpewebkit_minibrowser" else "--no-headless")
